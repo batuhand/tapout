9e7b31d7d55301d32ed61dd2983697a255c4d5a6
Batuhan Demircan <batuhandemircann@gmail.com>
2019-08-21 16:45:14 +0300

    Home page tasks

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,29 +10,34 @@ class HomePage extends StatelessWidget {
   final newTaskDescController = new TextEditingController();
   HomePage({Key key, @required this.username}) : super(key: key);
 
-
-
-
-  Future<List<Tasks>> _fetchTasks(String add) async{
+  Future<List<Tasks>> _fetchTasks(String add) async {
     final String uri = "https://tapoutapi.azurewebsites.net/api/tasks/" + add;
     print(uri);
     var response = await http.get(uri);
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       final tasks = json.decode(response.body).cast<Map<String, dynamic>>();
-      List<Tasks> listOfTasks = tasks.map<Tasks>((json){
+      List<Tasks> listOfTasks = tasks.map<Tasks>((json) {
         return Tasks.fromJson(json);
       }).toList();
 
-      return listOfTasks;
-    }else{
+      List<Tasks> unfinished = [];
+      for(int i=0; i<listOfTasks.length; i++){
+        if(listOfTasks[i].isCompleted == false){
+          unfinished.add(listOfTasks[i]);
+        }
+      }
+      print(listOfTasks[0].id);
+
+      return unfinished;
+    } else {
       throw Exception("Failed to load internet");
     }
-  }  
+  }
 
-@override
+  @override
   Widget build(BuildContext context) {
     print(username);
-        return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text("tapout"),
         centerTitle: true,
@@ -40,117 +45,127 @@ class HomePage extends StatelessWidget {
       ),
       body: FutureBuilder<List<Tasks>>(
         future: _fetchTasks(username),
-        builder: (context, snapshot){
-          if(!snapshot.hasData) return Center(child: CircularProgressIndicator());
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(child: CircularProgressIndicator());
 
           return ListView(
-            children: snapshot.data.map((item) => Card(
-              borderOnForeground: true,
-
-              child: ListTile(
-                title: Text(item.taskName),
-                subtitle: Text(item.taskDesc),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailScreen(todo: item),
+            children: snapshot.data
+                .map((item) => Card(
+                      borderOnForeground: true,
+                      child: ListTile(
+                        title: Text(item.taskName),
+                        subtitle: Text(item.taskDesc),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DetailScreen(todo: item),
+                            ),
+                          );
+                        },
                       ),
-                    );
-                  },
-              ),
-           
-            )).toList(),
+                    ))
+                .toList(),
           );
         },
       ),
       backgroundColor: Colors.red[500],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    content: Form(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: TextFormField(
-                              controller: newTaskNameController,
-                              keyboardType: TextInputType.text,
-                              cursorColor: Colors.red,
-                              autofocus: false,
-                              decoration: InputDecoration(
-                                hintText: "Task Name",
-                                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32)),
-                              ),
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Form(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: newTaskNameController,
+                            keyboardType: TextInputType.text,
+                            cursorColor: Colors.red,
+                            autofocus: false,
+                            decoration: InputDecoration(
+                              hintText: "Task Name",
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(20, 10, 20, 10),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(32)),
                             ),
                           ),
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: TextFormField(
-                              controller: newTaskDescController,
-                              keyboardType: TextInputType.text,
-                              cursorColor: Colors.red,
-                              autofocus: false,
-                              decoration: InputDecoration(
-                                hintText: "Task Description",
-                                contentPadding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32)),
-                              ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: newTaskDescController,
+                            keyboardType: TextInputType.text,
+                            cursorColor: Colors.red,
+                            autofocus: false,
+                            decoration: InputDecoration(
+                              hintText: "Task Description",
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(20, 10, 20, 10),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(32)),
                             ),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: RaisedButton(
-                              child: Text("Create new Task"),
-                              onPressed: (){
-                                  if(newTaskNameController.text != null && newTaskDescController.text != null){
-                                    createTask(username,newTaskNameController.text.toString(),newTaskDescController.text.toString());
-                                  }
-                                },
-                            ),
-                          )
-                        ],
-                      ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: RaisedButton(
+                            child: Text("Create new Task"),
+                            onPressed: () {
+                              if (newTaskNameController.text != null &&
+                                  newTaskDescController.text != null) {
+                                createTask(
+                                    username,
+                                    newTaskNameController.text.toString(),
+                                    newTaskDescController.text.toString());
+                              }
+                            },
+                          ),
+                        )
+                      ],
                     ),
-                  );
-                });
-          },
-        child: Icon(Icons.loupe,color: Colors.red,),
-        backgroundColor: Colors.white,
+                  ),
+                );
+              });
+        },
+        child: Icon(
+          Icons.loupe,
+          color: Colors.red,
         ),
+        backgroundColor: Colors.white,
+      ),
     );
- 
   }
 }
 
-createTask(String username, String taskName, String taskDesc){
-    var body = jsonEncode(
-      {	"taskName" : taskName,
-	      "taskDesc" : taskDesc,
-        });
+createTask(String username, String taskName, String taskDesc) {
+  var body = jsonEncode({
+    "taskName": taskName,
+    "taskDesc": taskDesc,
+  });
 
-    var uri = "https://tapoutapi.azurewebsites.net/api/user/" + username;
-    try{  
-      http.put(uri,body: body,headers: {"content-type":"application/json"}).then((response){
-        var result = json.decode(response.body);
-        //print(result["token"]);
-        print("Creating");
-        print(result);
-      });
-    }catch(e){
-      print("error");
-      }
-
-
+  var uri = "https://tapoutapi.azurewebsites.net/api/user/" + username;
+  try {
+    http.put(uri,
+        body: body,
+        headers: {"content-type": "application/json"}).then((response) {
+      var result = json.decode(response.body);
+      //print(result["token"]);
+      print("Creating");
+      print(result);
+    });
+  } catch (e) {
+    print("error");
+  }
 }
 
-
-class Tasks{
+class Tasks {
   int id;
   String taskName;
   String taskDesc;
@@ -165,7 +180,7 @@ class Tasks{
     this.taskUser,
   });
 
-  factory Tasks.fromJson(Map<String, dynamic> json){
+  factory Tasks.fromJson(Map<String, dynamic> json) {
     return Tasks(
       id: json["id"],
       taskName: json["taskName"],
